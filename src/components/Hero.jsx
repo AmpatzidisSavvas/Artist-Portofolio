8941d19a9d264cc40469144f594914cd13ff54c1
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-12-05 15:52:16 +0200

    Project Portofolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,203 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Button from './Button';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/all';
+import { PiFacebookLogo, PiInstagramLogo } from 'react-icons/pi';
+import { FaVimeo } from 'react-icons/fa';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
+
+  // 0 % 4 = 0 + 1 => 1
+  // 1 % 4 = 1 + 1 => 2
+  // 2 % 4 = 2 + 1 => 3
+  // 3 % 4 = 3 + 1 => 4
+  // 4 % 4 = 0 + 1 => 1
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+
+        gsap.set("#next-video", { visibility: "visible" });
+
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP( () => {
+    gsap.set('#video-frame', {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: ' 0 0 40% 10%'
+    })
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  })
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div id='home' className=" relative h-dvh w-screen overflow-x-hidden">
+      {loading && (
+        <div className=' flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50"'>
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className=" mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVdClick}
+              className=" origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className=" size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className=" absolute-center invisible z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className=" absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          Portofolio
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className=" mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              savvas ampatzidis
+            </h1>
+            <p className="mb-1 max-x-64 font-robert-regular text-blue-100">
+              Multidisciplinary Visual Designer
+            </p>
+            <p className="mb-5 max-x-64 font-robert-regular text-blue-100">
+              Visual Storyteller
+            </p>
+
+            <div className=" flex gap-2">
+              <Button
+                id="watch-trailer"
+                title="Vimeo"
+                leftIcon={<FaVimeo />}
+                onClick={() =>
+                  (window.location.href = "https://vimeo.com/user85100118")
+                }
+                containerClass="!bg-yellow-300 flex-center gap-1"
+              />
+              <Button
+                id="watch-trailer"
+                title="Instagram"
+                leftIcon={<PiInstagramLogo />}
+                onClick={() =>
+                  (window.location.href =
+                    "https://www.instagram.com/thepack_art_/")
+                }
+                containerClass="!bg-yellow-300 flex-center gap-1"
+              />
+              <Button
+                id="watch-trailer"
+                title="Facebook"
+                leftIcon={<PiFacebookLogo />}
+                onClick={() =>
+                  (window.location.href =
+                    "https://www.facebook.com/ThePackArte/")
+                }
+                containerClass="!bg-yellow-300 flex-center gap-1"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+        Portofolio
+      </h1>
+    </div>
+  );
+};
+
+export default Hero
---
+++ src/components/Projects.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import Card from './Card';
+import AnimatedTitle from "./AnimatedTitle"
+import Tilt from './Tilt'
+
+
+const Projects = () => {
+
+  return (
+    <section id='projects' className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <AnimatedTitle title="Projects" />
+        </div>
+        <Tilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh] ">
+          <Card
+            src="videos/feature-1.mp4"
+            title={<>"dialleima"</>}
+            description={
+              <>
+                3D animated music video for the artist "Agrimi". showcasing a
+                dynamic blend of visuals and music.
+              </>
+            }
+            titleButton="Watch full video"
+            onClick={() =>
+              (window.location.href =
+                "https://www.youtube.com/watch?v=57m9Sb6Nk4M")
+            }
+          />
+        </Tilt>
+
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <Tilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <Card
+              title={<>creatures</>}
+              description={
+                <>Creatures from my animated short film "Unusual Mind".</>
+              }
+              src="videos/feature-2.mp4"
+            />
+          </Tilt>
+          <Tilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <Card title={<>flying mountains</>} src={"videos/feature-3.mp4"} />
+          </Tilt>
+          <Tilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <Card
+              title={<>"go-ship"</>}
+              description={
+                <>3D animated music video for the artist "Silent".</>
+              }
+              src={"videos/feature-4.mp4"}
+              titleButton="Watch full video"
+              onClick={() =>
+                (window.location.href =
+                  "https://www.youtube.com/watch?v=kZOZSruDpRw")
+              }
+            />
+          </Tilt>
+
+          {/* Double small cards */}
+          
+          <Tilt className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
+          </Tilt>
+          <Tilt className="bento-tilt_2">
+            <video
+              src="videos/feature-6.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
+          </Tilt>
+          
+          
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Projects
