6b2c11fa1733e25b31499442b4d4dc9995a54181
AmpatzidisSavvas <savvas.ampa@gmail.com>
2025-06-29 09:54:36 +0300

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -82,7 +82,7 @@ const Projects = () => {
 
 				<div className="grid  grid-cols-1 md:grid-cols-2 mt-7 gap-7">
 					<SmallCard src={"videos/feature-10.mp4"} />
-					{/* <SmallCard src={"videos/feature-11.mp4"} /> */}
+					<SmallCard src={"videos/feature-11.mp4"} />
 				</div>
 			</div>
 			<div className="px-5 py-10">
