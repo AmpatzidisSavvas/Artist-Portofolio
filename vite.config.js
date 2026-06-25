ef86991d95054a2f1585c657d5701323b5caa024
AmpatzidisSavvas <savvas.ampa@gmail.com>
2026-06-25 11:51:55 +0300

    Fix deploy for both Vercel and github page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-	plugins: [react()],
-	base: mode === "production" ? "Artist-Portofolio" : "/",
-	server: {
-		open: true
-	}
-}));
+export default defineConfig({
+	plugins: [react()]
+});
