d6464079d947ccd985eaa503e00468754a078e57
AmpatzidisSavvas <AmpatzidisSavvas@users.noreply.github.com>
2026-08-26 07:05:52 +0000

    deploy: 0ec68c030043a6dd026ebc22486d5d1e7d8ff5d2

--- assets/Hero-BXAsYxCf.js
+++ assets/Hero-BXAsYxCf.js
@@ -1,4 +1,4 @@
-import{a as e,j as t}from"./index-B5-kUyzJ.js";import{g as N}from"./index-DSpwt0cg.js";import{m as T}from"./proxy-Ddz9oc52.js";import{S as V}from"./ScrollTrigger-CezCZ8EY.js";/*!
+import{a as e,j as t}from"./index-BqGw8x2F.js";import{g as N}from"./index-DSpwt0cg.js";import{m as T}from"./proxy-Rlc8YKni.js";import{S as V}from"./ScrollTrigger-CezCZ8EY.js";/*!
  * @gsap/react 2.1.2
  * https://gsap.com
  *
